ff41adf70db92302a953dfb50f07b1dc7387bf03
olavgranberg <57437051+olavgranberg@users.noreply.github.com>
2019-11-21 12:57:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -434,8 +434,10 @@ let waitingRoomUpdate = document.getElementById("waitingRoomUpdate");
 
 waitingRoomUpdate .addEventListener('click', function() {
 
+  var txt= webstrate.clientId.value();
+
   var node = document.createElement("LI");                 // Create a <li> node
-  var textnode = document.createTextNode(webstrate.clientId.innerHTML);         // Create a text node
+  var textnode = document.createTextNode(txt);         // Create a text node
   node.appendChild(textnode);                              // Append the text to <li>
   document.getElementById("waitingRoomList").appendChild(node);
 
