0688c08440170bf2745e9122f71a93122fbfb445
olavgranberg <57437051+olavgranberg@users.noreply.github.com>
2019-12-02 15:34:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -93,6 +93,6 @@ webstrate.on("clientPart", function(clientId) {
 
   document.getElementById("testButton").addEventListener("click", function() {
 
-alert(document.getElementById("iframeAnalyst").href.value;
+alert(document.getElementById("iframeAnalyst").href.value);
 
 });
