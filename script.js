04fecfe50c2de9233bb8dafdd4cac3e44badcd69
olavgranberg <57437051+olavgranberg@users.noreply.github.com>
2019-12-02 12:06:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ webstrate.on("loaded", function(webstrateId, clientId, user) {
 });
 
 
-webstrate.on("clientPart", function(clientId) {
+webstrate.on("disconnect", function(clientId) {
   let partingUser = document.getElementById(clientId);
 	console.log(partingUser, clientId);
 	partingUser.parentNode.removeChild(partingUser);
