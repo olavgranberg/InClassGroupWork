6168d50050e6d9b993c8178fb3c7ca0ff340b369
olavgranberg <57437051+olavgranberg@users.noreply.github.com>
2019-12-04 11:15:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,26 +2,6 @@
 let userId = Math.floor(Math.random() * 4) + 1;
 
 
-// If sentence that defines the users specific group role with a header and a paragraph
-
-/*
-let roleDivH3 = document.getElementById("roleDivHeader");
-let roleDivP = document.getElementById("roleDivDescription");
-if (userId===1){
-  roleDivH3.innerHTML = "References responsible";
-  roleDivP.innerHTML = "The job is to ensure that the needed litteratur, references and quotes are added to the project. Making the answers trustworthy and reliable.";
-} else if (userId===2){
-  roleDivH3.innerHTML = "Analyst";
-  roleDivP.innerHTML = "The analysts roles is to note findings and subjects for further discussion during the collaborative open discussions. Furthermore the analysts perspective must be critic and nuanced.";
-}else if (userId===3){
-  roleDivH3.innerHTML = "Secretary";
-  roleDivP.innerHTML = "A secretary is responsible for taking notes of relevant points from the discussion and writing down the answers for the questions. The note-take can only answer the available alphabetic/numeric bullet points referring to the lectors questions.";
-}else{
-  roleDivH3.innerHTML = "Presenter";
-  roleDivP.innerHTML = "A presenters role is to present the groups collaborative outcome/result. He can edit the “presenters textfield” but only add short sentences according to bullet points format";
-}
-
-*/
 
 
 // ------------------------------ getusername function ----------------------
@@ -46,46 +26,6 @@ webstrate.on("clientPart", function(clientId) {
 
 
 
-
-/*
-// ----------------------------- Client ids -------------------
-  document.getElementById("waitingRoomUpdate").addEventListener("click", function() {
-
-
-//--------------------------- create iframes -------------------------
-
-
- let workspaceDiv = document.getElementById("workspace");
-
- let iframeAnswer = document.createElement("iframe");
- iframeAnswer.setAttribute("src", "https://webstrates.cs.au.dk/kais2019inclassgroupworkanswers/?copy");
- iframeAnswer.setAttribute("id", "iframeAnswer");
- workspaceDiv.appendChild(iframeAnswer);
-
-
-
- let iframePresenter = document.createElement("iframe");
- iframePresenter.setAttribute("src", "https://webstrates.cs.au.dk/kais2019inclassgroupworkpresenter/?copy");
- iframePresenter.setAttribute("id", "iframePresenter");
- workspaceDiv.appendChild(iframePresenter);
-
-
-
- let iframeAnalyst = document.createElement("iframe");
- iframeAnalyst.setAttribute("src", "https://webstrates.cs.au.dk/kais2019inclassgroupworkanalyst/?copy");
- iframeAnalyst.setAttribute("id", "iframeAnalyst");
- workspaceDiv.appendChild(iframeAnalyst);
-
-
-
- let iframeReference = document.createElement("iframe");
- iframeReference.setAttribute("src", "https://webstrates.cs.au.dk/kais2019inclassgroupworkreference/?copy");
- iframeReference.setAttribute("id", "iframeReference");
- workspaceDiv.appendChild(iframeReference);
-
-
-});
-*/
 
 
   document.getElementById("referenceRoleUnderstood").addEventListener("click", function() {
@@ -114,10 +54,8 @@ webstrate.on("clientPart", function(clientId) {
 
   document.getElementById("analystRoleUnderstood").addEventListener("click", function(user) {
 
-    let username = user.userId;
-
     let iframeAnalystUpdated = document.getElementById('iframeAnalyst').contentDocument.location;
-  document.getElementById('iframeAnalyst').setAttribute("src", iframeAnalystUpdated + "?username=" + username);
+  document.getElementById('iframeAnalyst').setAttribute("src", iframeAnalystUpdated + "?username=" + user.userId);
 
 
 
@@ -140,3 +78,67 @@ webstrate.on("clientPart", function(clientId) {
 
 
   });
+
+
+  // If sentence that defines the users specific group role with a header and a paragraph
+
+  /*
+  let roleDivH3 = document.getElementById("roleDivHeader");
+  let roleDivP = document.getElementById("roleDivDescription");
+  if (userId===1){
+    roleDivH3.innerHTML = "References responsible";
+    roleDivP.innerHTML = "The job is to ensure that the needed litteratur, references and quotes are added to the project. Making the answers trustworthy and reliable.";
+  } else if (userId===2){
+    roleDivH3.innerHTML = "Analyst";
+    roleDivP.innerHTML = "The analysts roles is to note findings and subjects for further discussion during the collaborative open discussions. Furthermore the analysts perspective must be critic and nuanced.";
+  }else if (userId===3){
+    roleDivH3.innerHTML = "Secretary";
+    roleDivP.innerHTML = "A secretary is responsible for taking notes of relevant points from the discussion and writing down the answers for the questions. The note-take can only answer the available alphabetic/numeric bullet points referring to the lectors questions.";
+  }else{
+    roleDivH3.innerHTML = "Presenter";
+    roleDivP.innerHTML = "A presenters role is to present the groups collaborative outcome/result. He can edit the “presenters textfield” but only add short sentences according to bullet points format";
+  }
+
+  */
+
+
+
+  /*
+  // ----------------------------- Client ids -------------------
+    document.getElementById("waitingRoomUpdate").addEventListener("click", function() {
+
+
+  //--------------------------- create iframes -------------------------
+
+
+   let workspaceDiv = document.getElementById("workspace");
+
+   let iframeAnswer = document.createElement("iframe");
+   iframeAnswer.setAttribute("src", "https://webstrates.cs.au.dk/kais2019inclassgroupworkanswers/?copy");
+   iframeAnswer.setAttribute("id", "iframeAnswer");
+   workspaceDiv.appendChild(iframeAnswer);
+
+
+
+   let iframePresenter = document.createElement("iframe");
+   iframePresenter.setAttribute("src", "https://webstrates.cs.au.dk/kais2019inclassgroupworkpresenter/?copy");
+   iframePresenter.setAttribute("id", "iframePresenter");
+   workspaceDiv.appendChild(iframePresenter);
+
+
+
+   let iframeAnalyst = document.createElement("iframe");
+   iframeAnalyst.setAttribute("src", "https://webstrates.cs.au.dk/kais2019inclassgroupworkanalyst/?copy");
+   iframeAnalyst.setAttribute("id", "iframeAnalyst");
+   workspaceDiv.appendChild(iframeAnalyst);
+
+
+
+   let iframeReference = document.createElement("iframe");
+   iframeReference.setAttribute("src", "https://webstrates.cs.au.dk/kais2019inclassgroupworkreference/?copy");
+   iframeReference.setAttribute("id", "iframeReference");
+   workspaceDiv.appendChild(iframeReference);
+
+
+  });
+  */
