0832df8158c40679d698349ee2a46b0ffc711948
olavgranberg <57437051+olavgranberg@users.noreply.github.com>
2019-12-04 10:47:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -116,7 +116,7 @@ webstrate.on("clientPart", function(clientId) {
 
 
     let iframeAnalystUpdated = document.getElementById('iframeAnalyst').contentDocument.location;
-  document.getElementById('iframeAnalyst').setAttribute("src", iframeAnalystUpdated + "?username=" + "hej");
+  document.getElementById('iframeAnalyst').setAttribute("src", iframeAnalystUpdated + "?username=");
 
 
 
