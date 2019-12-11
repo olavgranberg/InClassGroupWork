632bd03299f538bdde21e424f4a9571d8a5b37f3
olavgranberg <57437051+olavgranberg@users.noreply.github.com>
2019-12-11 14:23:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -71,7 +71,8 @@ document.getElementById("delegateRoles").addEventListener("click", function() {
 var ul = document.querySelector('ul');
 for (var i = ul.children.length; i >= 0; i--) {
     ul.appendChild(ul.children[Math.random() * i | 0]);
-};
+}
+});
 
 
 
